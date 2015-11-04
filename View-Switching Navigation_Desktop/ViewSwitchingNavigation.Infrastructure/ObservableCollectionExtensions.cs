af2a44f357e9c3336ef535b7cceee3c1ff7c2d7a
Yehudah Altman <yealter@gmail.com>
2015-11-05 01:01:06 +0200

    Add ObservableCollectionExtensions
    provides AddRange method to ObservableCollection.

---
+++ View-Switching Navigation_Desktop/ViewSwitchingNavigation.Infrastructure/ObservableCollectionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace ViewSwitchingNavigation.Infrastructure
+{
+    public static class ObservableCollectionExtensions
+    {
+        public static void AddRange<T> (this ObservableCollection<T> targetCollection,IEnumerable<T> source)
+        {
+            foreach(T item in source)
+            {
+                targetCollection.Add(item);
+            }
+        }
+    }
+}
